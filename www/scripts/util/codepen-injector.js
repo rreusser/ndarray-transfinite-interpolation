7641a7f0c7f7e16e6de5bd58f74f0207493256e1
Ricky Reusser <rsreusser@gmail.com>
2016-08-22 15:25:06 -0400

    Cube example

--- www/scripts/util/codepen-injector.js
+++ www/scripts/util/codepen-injector.js
@@ -12,14 +12,15 @@ function codepenInjector (content, externalJS) {
   if (content.js) data.js = content.js;
   if (content.css) data.css = content.css;
   if (externalJS) data.js_external = externalJS;
+  data.editors = '0010';
 
   var JSONstring = JSON.stringify(data).replace(/"/g, '&quot;');
 
-  var formStyle = 'display:inline-block;position:absolute;top:0;right:5px;'
+  var formStyle = 'display:inline-block;position:absolute;bottom:5px;right:5px;z-index:1000;'
   var form =
     '<form action="http://codepen.io/pen/define" method="POST" target="_blank" style="' + formStyle + '">' +
       '<input type="hidden" name="data" value="' + JSONstring + '">' +
-      '<input type="image" src="http://s.cdpn.io/3/cp-arrow-right.svg" width="40" height="40" value="Create New Pen with Prefilled Data" class="codepen-mover-button">' +
+      '<input type="image" src="http://s.cdpn.io/3/cp-arrow-right.svg" width="40" height="40" style="background-color:#333;border-radius:3px;padding:0 10px;" value="Create New Pen with Prefilled Data" class="codepen-mover-button">' +
     '</form>';
 
   return hyperstream({
